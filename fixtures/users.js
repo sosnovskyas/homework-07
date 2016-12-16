529a1eadd1815887075429ce5979ba909a120768
Sosnovsky Alexey <sosnovsky.as@gmail.com>
2016-12-16 21:20:44 +0300

    db: added fixtures

--- fixtures/users.js
+++ fixtures/users.js
@@ -2,13 +2,13 @@ const oid = require('../libs/oid');
 require('../models/user');
 
 exports.User = [{
-  _id:      oid('user-mk'),
-  email:    "mk@javascript.ru",
+  _id: oid('user-mk'),
+  email: 'mk@javascript.ru',
   displayName: 'mk',
   password: '123456'
 }, {
-  _id:      oid('user-iliakan'),
-  email:    "iliakan@javascript.ru",
+  _id: oid('user-iliakan'),
+  email: 'iliakan@javascript.ru',
   displayName: 'iliakan',
   password: '123456'
 }];
