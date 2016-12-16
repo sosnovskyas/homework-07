67a73d717d4c6494da1afc0ded60bea245dab5aa
Sosnovsky Alexey <sosnovsky.as@gmail.com>
2016-12-17 00:34:25 +0300

    db: test updated

--- test/server.js
+++ test/server.js
@@ -1,5 +1,6 @@
 'use strict';
 
+const path = require('path');
 const should = require('should');
 
 const app = require('../app');
@@ -9,9 +10,9 @@ const request = require('request-promise').defaults({
   simple: false
 });
 
-const clearDatabase = require('../libs/clearDatabase');
-
 const serverPath = `http://${config.host}:${config.port}`;
+const fixturesPath = path.join(__dirname, `../fixtures/users`);
+
 let server;
 
 describe('server', () => {
@@ -39,19 +40,39 @@ describe('server', () => {
   });
 
   describe('REST API', () => {
-    const mongoose = require('mongoose');
+    const mongoose = require('../libs/mongoose');
 
-    beforeEach(async ()=>{
-      await clearDatabase();
+    beforeEach(async() => {
+      await mongoose.connection.dropDatabase();
+
+      let Users;
+      try {
+        Users = mongoose.model('User');
+      } catch (error) {
+        Users = require('../models/user');
+      }
+      await Promise.all(
+        require('../fixtures/users').User // users array
+          .map(
+            item => {
+              let newUser = new Users(item);
+              return newUser.save();
+            }
+          )
+      );
     });
 
-    afterEach(async ()=>{
-      mongoose.connection.close();
+
+    afterEach(async() => {
+      await mongoose.model('User').find({}, (err, res) => {
+        console.log(res)
+      });
+      await mongoose.connection.close();
     });
 
     context('users', () => {
-      it(`GET ${serverPath}/users response status 200`, async() => {
-      /*it(`GET ${serverPath}/users response status 200 and list defaul users`, async() => {*/
+      // it(`GET ${serverPath}/users response status 200`, async() => {
+      it(`GET ${serverPath}/users response status 200 and users list`, async() => {
         let response = await request({
           method: 'get',
           uri: `${serverPath}/users`,
@@ -59,6 +80,18 @@ describe('server', () => {
         });
 
         should(response.statusCode).eql(200);
+        should(response.body).eql([
+          {
+            _id: '31ef97a095aa7859d9c6f43e',
+            email: 'mk@javascript.ru',
+            displayName: 'mk',
+          },
+          {
+            _id: 'a5c9a78e68a0f5a85275ef53',
+            email: 'iliakan@javascript.ru',
+            displayName: 'iliakan',
+          }
+        ]);
       });
     });
   });
