9102aaaa8dc7850cd7c9689a58c7f5ae530e3d65
Sosnovsky Alexey <sosnovsky.as@gmail.com>
2016-12-18 18:22:25 +0300

    passport: changed route, added auth chech

--- routes/get.root.js
+++ routes/get.root.js
@@ -1,14 +1,12 @@
 'use strict';
 
 const handler = async(ctx) => {
-  //TODO: реализовать функционал проверки авторизации
-  // if (ctx.isAuthenticated()) {
-  //   ctx.body = ctx.render('welcome');
-  // } else {
-  ctx.body = ctx.render('login');
-  // }
+  if (ctx.isAuthenticated()) {
+    ctx.body = ctx.render('welcome');
+  } else {
+    ctx.body = ctx.render('login');
+  }
 };
-
 exports.route = {
   metod: 'get',
   path: '/',
