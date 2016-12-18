f327603add2926acb7126eb8c6023a7b76b137a8
Sosnovsky Alexey <sosnovsky.as@gmail.com>
2016-12-18 19:12:36 +0300

    passport: changed frontend

--- public/bundle.js
+++ public/bundle.js
@@ -2,19 +2,53 @@
 
 const loginForm = document.querySelector('.login-form');
 const registerForm = document.querySelector('.register-form');
+const onLoginSubmit = event => {
+  // const target = event.target;
+  event.preventDefault();
+  new Promise(function (resolve, reject) {
 
-if (loginForm) {
-  loginForm.addEventListener('submit', event => {
-    // const target = event.target;
-    event.preventDefault();
+    const xhr = new XMLHttpRequest();
+    xhr.open('POST', '/login', true);
 
+    xhr.onload = function () {
+      if (this.status == 200) {
+        resolve(this.response);
+      } else {
+        const error = new Error(this.statusText);
+        error.code = this.status;
+        reject(error);
+      }
+    };
+
+    xhr.onerror = function () {
+      reject(new Error('Network Error'));
+    };
+
+    xhr.send();
   });
-}
+};
+const onRegisterSubmit = event => {
+  // const target = event.target;
+  event.preventDefault();
+  const xhr = new XMLHttpRequest();
+  xhr.open('POST', '/register', true);
 
-if (registerForm) {
-  registerForm.addEventListener('submit', event => {
-    // const target = event.target;
-    event.preventDefault();
+  xhr.onload = function () {
+    if (this.status == 200) {
+      console.log(this.response);
+    } else {
+      const error = new Error(this.statusText);
+      error.code = this.status;
+      console.warn(error);
+    }
+  };
 
-  });
-}
+  xhr.onerror = function () {
+    console.warn(new Error('Network Error'));
+  };
+
+  xhr.send();
+};
+
+if (loginForm) loginForm.addEventListener('submit', onLoginSubmit);
+if (registerForm) registerForm.addEventListener('submit', onRegisterSubmit);
