feaf5e60487594b86adb247bbc75f5836e304a3e
Sosnovsky Alexey <sosnovsky.as@gmail.com>
2016-12-18 19:04:08 +0300

    passport: changed frontend

--- public/bundle.js
+++ public/bundle.js
@@ -1,9 +1,19 @@
 'use strict';
 
 const loginForm = document.querySelector('.login-form');
+const registerForm = document.querySelector('.register-form');
 
 if (loginForm) {
   loginForm.addEventListener('submit', event => {
+    // const target = event.target;
+    event.preventDefault();
+
+  });
+}
+
+if (registerForm) {
+  registerForm.addEventListener('submit', event => {
+    // const target = event.target;
     event.preventDefault();
 
   });
