295e416dd53ead95ded15b692f2a214c61316b0c
Sosnovsky Alexey <sosnovsky.as@gmail.com>
2016-12-19 15:13:15 +0300

    passport: modified send messages

--- routes/register/post.js
+++ routes/register/post.js
@@ -2,10 +2,11 @@
 
 const dbApi = require('../../libs/dbApi');
 const config = require('config');
-const sendMail = require('../../libs/sendMail');
+// const sendMail = require('../../libs/sendMail');
+const nodemailer = require('nodemailer');
+
 
 const handler = async(ctx) => {
-  console.log(ctx.request.body);
   let User = dbApi.getModel('user');
   const verifyEmailToken = Math.random().toString(36).slice(2, 10);
   const user = new User({
@@ -36,12 +37,30 @@ const handler = async(ctx) => {
 
   // We're here if no errors happened
 
-  await sendMail({
-    template: 'verify-registration-email',
+  // await sendMail({
+  //   template: 'verify-registration-email',
+  //   to: user.email,
+  //   subject: 'Подтверждение email',
+  //   link: config.app.host + '/verify-email/' + verifyEmailToken
+  // });
+
+
+  const sendMailTransport = require('nodemailer-sendmail-transport');
+  const transport = nodemailer.createTransport(
+    sendMailTransport({})
+  );
+  const params = {
+    from: 'lvlonstradamus@gmail.com',
     to: user.email,
-    subject: 'Подтверждение email',
-    link: config.app.host + '/verify-email/' + verifyEmailToken
-  });
+    subject: 'Hi, body!',
+    text: `${config.app.host}/verify-email/${verifyEmailToken}`
+  };
+// transport.sendMail(params, function (err, res) {
+//     if (err) {
+//       console.error(err);
+//     }
+//   });
+  await transport.sendMail(params);
 
   ctx.body = 'Вы зарегистрированы. Пожалуйста, загляните в почтовый ящик, там письмо с Email-подтверждением.';
 };
